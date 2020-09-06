557062c74ca24c1092196291cbe7db757ef7df71
Ahmad Jarara <ajarara94@gmail.com>
2020-09-06 12:31:18 -0400

    Add lab for snackbar attempt, pull out full tab view
    As a component, so we can add additional things without polluting the
    main App component. We can also easily switch on smaller components.

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 import Tabs from '@material-ui/core/Tabs' ;
 import Tab from '@material-ui/core/Tab';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-
-function App() {
-  
+const FullTabView = () => {
   const [value, setValue] = useState<number>(0);
-
+  
   const onChange = useCallback((_, value) => {
     setValue(value);
   }, [setValue]);
+
+  return (
+    <>
+      <Tabs value={value} onChange={onChange}>
+        <Tab label={"Home"} />
+        <Tab label={"About"} />
+        <Tab label={"Projects"} />
+        <Tab label={"Team"} />
+        <Tab label={"Contact"} />
+      </Tabs>
+    </>
+  );
+}
+
+const App = () => {
+  const theme = useTheme();
+  const aboveLarge = useMediaQuery(theme.breakpoints.up('lg'));
   
   return (
     <div className="App">
-      <Tabs value={value} onChange={onChange}>
-      <Tab label={"Home"} />
-      <Tab label={"About"} />
-      <Tab label={"Project"} />
-      <Tab label={"Team"} />
-      <Tab label={"Contact"} />
-
-      </Tabs>
+      {aboveLarge && <FullTabView />}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
