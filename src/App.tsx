db8e51eb7185a6e7a846f49199039a6bec00d317
Mohammad <pr.mlz2020@gmail.com>
2020-09-13 17:07:44 +0300

    Latest_Upgrader

--- src/App.tsx
+++ src/App.tsx
@@ -50,18 +50,8 @@ const App = () => {
     <div className="App">
       {aboveLarge && <FullTabView />}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Welcome to Masarat </h1>
+
       </header>
     </div>
   );
