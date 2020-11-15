4e3f08209fd8c3160e3259940ebe0b9b60b63119
Ahmad Jarara <ajarara94@gmail.com>
2020-11-15 12:59:09 -0500

    Add card styles

--- src/About.tsx
+++ src/About.tsx
@@ -1,32 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { useMediaQuery, useTheme, makeStyles } from '@material-ui/core';
+import { useMediaQuery, useTheme, makeStyles, Theme } from '@material-ui/core';
 
-const useStyles = makeStyles ({
+interface CardStyleProps {
+  grid?: boolean,
+}
+
+const useStyles = makeStyles({
   aboutUsInput: { 
     paddingBottom : "12px" ,
     paddingTop: "2px",
     margin : "8px"
-
-    
   },
   title:{
     margin:"2px",
     fontSize: "20px" ,
     textAlign: "center",
-
-
-
   },
   aboutUsForm:{
     display: "flex", flexDirection: "column" , padding: "16px", margin: "8px",
-  }
-})
+  },
+});
+
+const useCardStyles = makeStyles<Theme, CardStyleProps>((theme: Theme) => ({
+  aboutUsCard: ({ grid }) => ({
+    border: '0.01rem solid',
+    borderRadius: '3px',
+    textAlign: 'center',
+    width: grid ? '40%' : '20%',
+    maxWidth: '200px',
+    marginBottom: '10px',
+  }),
+}));
 
 const AboutUsCard: React.FC <{
   title: string,
   subtitle: string,
-}> = ({ title, subtitle }) => (
-  <div className={"About_Us_Card"}>
+  className: string,
+}> = ({ title, subtitle, className }) => (
+  <div className={className}>
     <p className={"About_Us_Card_Title"}>{title}</p>
     <p className={"About_Us_Card_Subtitle"}>{subtitle}</p>
     </div>
@@ -73,29 +84,38 @@ const useTimedCount = () => {
 
 
 const AboutUsCardListingHorizontal: React.FC<{count: number}> = ({ count }) => {
+  const styles = useCardStyles({
+    grid: false,
+  });
+ 
   return <div className={"About_Us_Card_Listing"}>
-    <AboutUsCard title={`${Math.floor(count / 20)}`} subtitle={'YEARS OF EXPERIENCE'} />
-    <AboutUsCard title={`${Math.floor(count)}`} subtitle={'PROJECTS DONE'} />
-    <AboutUsCard title={`${Math.floor(count / 50)}`} subtitle={'TEAM'} />
-    <AboutUsCard title={`${Math.floor(count / 1.052631578947368)}`} subtitle={'HAPPY CUSTOMERS'} />
+    <AboutUsCard title={`${Math.floor(count / 20)}`} subtitle={'YEARS OF EXPERIENCE'} className={styles.aboutUsCard}/>
+    <AboutUsCard title={`${Math.floor(count)}`} subtitle={'PROJECTS DONE'} className={styles.aboutUsCard}/>
+    <AboutUsCard title={`${Math.floor(count / 50)}`} subtitle={'TEAM'} className={styles.aboutUsCard}/>
+    <AboutUsCard title={`${Math.floor(count / 1.052631578947368)}`} subtitle={'HAPPY CUSTOMERS'} className={styles.aboutUsCard}/>
   </div>
 };
 
 const AboutUsCardListingGrid: React.FC<{count: number}> = ({ count }) => {
+  const styles = useCardStyles({
+    grid: true,
+  });
+  
   return <>
     <div className={"About_Us_Card_Listing"}>
-      <AboutUsCard title={`${Math.floor(count / 20)}`} subtitle={'YEARS OF EXPERIENCE'} />
-      <AboutUsCard title={`${Math.floor(count)}`} subtitle={'PROJECTS DONE'} />
+      <AboutUsCard title={`${Math.floor(count / 20)}`} subtitle={'YEARS OF EXPERIENCE'} className={styles.aboutUsCard}/>
+      <AboutUsCard title={`${Math.floor(count)}`} subtitle={'PROJECTS DONE'} className={styles.aboutUsCard}/>
     </div>
     <div className={"About_Us_Card_Listing"}>
-      <AboutUsCard title={`${Math.floor(count / 50)}`} subtitle={'TEAM'} />
-      <AboutUsCard title={`${Math.floor(count / 1.052631578947368)}`} subtitle={'HAPPY CUSTOMERS'} />
+      <AboutUsCard title={`${Math.floor(count / 50)}`} subtitle={'TEAM'} className={styles.aboutUsCard}/>
+      <AboutUsCard title={`${Math.floor(count / 1.052631578947368)}`} subtitle={'HAPPY CUSTOMERS'} className={styles.aboutUsCard}/>
     </div>
   </>
 };
  // ABOUT US FORM 
 const AboutUsForm  = () => {
-  const styles = useStyles ()
+  const styles = useStyles();
+  
   return <div className = {styles.aboutUsForm}>
 
       <p className = {styles.title}>BE PART OF OUR BUSINESS</p>
